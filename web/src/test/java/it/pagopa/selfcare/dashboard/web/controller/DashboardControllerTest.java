37c6e14d197fb9b91cfa58ea1447b43564b083d0
mtedahfo <marve.dullain.tedah.fosso.st@everis.nttdata.com>
2021-10-28 12:34:00 +0200

    Add Web Module UnitTest

---
+++ web/src/test/java/it/pagopa/selfcare/dashboard/web/controller/DashboardControllerTest.java
@@ -0,0 +1,75 @@
+package it.pagopa.selfcare.dashboard.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.dashboard.connector.rest.model.party_mgmt.Organization;
+import it.pagopa.selfcare.dashboard.core.PartyManagementService;
+import it.pagopa.selfcare.dashboard.web.model.OrganizationResource;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(value = {DashboardController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@ContextConfiguration(classes = { DashboardController.class })
+class DashboardControllerTest {
+
+    private static final String BASE_URL = "/dashboard";
+
+    @MockBean
+    private PartyManagementService PartyManagementServiceMock;
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Test
+    void getOrganizationNotNull() throws Exception {
+        // given
+        Mockito.when(PartyManagementServiceMock.getOrganization(Mockito.anyString()))
+                .thenAnswer(invocationOnMock -> {
+                    String id = invocationOnMock.getArgument(0, String.class);
+                    Organization p = new Organization();
+                    p.setInstitutionId(id);
+                    return p;
+                });
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/id")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        OrganizationResource resource = objectMapper.readValue(result.getResponse().getContentAsString(), OrganizationResource.class);
+        assertNotNull(resource);
+    }
+
+    @Test
+    void getOrganizationNull() throws Exception {
+        // given
+        Mockito.when(PartyManagementServiceMock.getOrganization(Mockito.anyString()))
+                .thenReturn(null);
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/id")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        assertEquals("", result.getResponse().getContentAsString());
+    }
+
+}
---
+++ web/src/test/java/it/pagopa/selfcare/dashboard/web/model/mapper/OrganizationMapperTest.java
@@ -0,0 +1,36 @@
+package it.pagopa.selfcare.dashboard.web.model.mapper;
+
+import it.pagopa.selfcare.commons.utils.TestUtils;
+import it.pagopa.selfcare.dashboard.connector.rest.model.party_mgmt.Organization;
+import it.pagopa.selfcare.dashboard.web.model.OrganizationResource;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationMapperTest {
+
+    @Test
+    void toResourceNotNull() {
+        // given
+        Organization organization = TestUtils.mockInstance(new Organization());
+        // when
+        OrganizationResource resource = OrganizationMapper.toResource(organization);
+        // then
+        assertEquals(organization.getInstitutionId(), resource.getId());
+        assertEquals(null, resource.getLogo());//TODO
+        assertEquals(null, resource.getOrganizationType());//TODO
+        assertEquals(organization.getDescription(), resource.getOrganizationName());
+        assertEquals(null, resource.getFiscalCode());//TODO
+        assertEquals(organization.getDigitalAddress(), resource.getMailAddress());
+        assertEquals(null, resource.getIPACode());//TODO
+        TestUtils.reflectionEqualsByName(organization, resource);
+    }
+
+    @Test
+    void toResourceNull() {
+        // given and when
+        OrganizationResource organizationResource = OrganizationMapper.toResource(null);
+        // then
+        assertNull(organizationResource);
+    }
+}
