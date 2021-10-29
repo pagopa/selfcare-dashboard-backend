b11d6de8329927f309bcbb4049238d6e99a444f3
mtedahfo <marve.dullain.tedah.fosso.st@everis.nttdata.com>
2021-10-29 18:16:36 +0200

    Add WEB Module Products

---
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/model/ProductsResource.java
@@ -0,0 +1,24 @@
+package it.pagopa.selfcare.dashboard.web.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class ProductsResource {
+    @ApiModelProperty("${swagger.products.model.id}")
+    private String id;
+    @ApiModelProperty("${swagger.products.model.logo}")
+    private String logo;
+    @ApiModelProperty("${swagger.products.model.title}")
+    private String title;
+    @ApiModelProperty("${swagger.products.model.description}")
+    private String description;
+    @ApiModelProperty("${swagger.products.model.urlPublic}")
+    private String urlPublic;
+    @ApiModelProperty("${swagger.products.model.urlBO}")
+    private String urlBO;
+    @ApiModelProperty("${swagger.products.model.activationDateTime}")
+    private OffsetDateTime activationDateTime;
+}
--- web/src/test/java/it/pagopa/selfcare/dashboard/web/controller/DashboardControllerTest.java
+++ web/src/test/java/it/pagopa/selfcare/dashboard/web/controller/DashboardControllerTest.java
@@ -1,9 +1,14 @@
 package it.pagopa.selfcare.dashboard.web.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.commons.utils.TestUtils;
 import it.pagopa.selfcare.dashboard.connector.rest.model.party_mgmt.Organization;
-import it.pagopa.selfcare.dashboard.core.PartyManagementService;
+import it.pagopa.selfcare.dashboard.connector.rest.model.products.Product;
+import it.pagopa.selfcare.dashboard.core.party_mgmt.PartyManagementService;
+import it.pagopa.selfcare.dashboard.core.products.ProductsService;
 import it.pagopa.selfcare.dashboard.web.model.OrganizationResource;
+import it.pagopa.selfcare.dashboard.web.model.ProductsResource;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(value = {DashboardController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
@@ -24,6 +32,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class DashboardControllerTest {
 
     private static final String BASE_URL = "/dashboard";
+    private static final Product PRODUCT = TestUtils.mockInstance(new Product());
+
+    @MockBean
+    private ProductsService productsService;
 
     @MockBean
     private PartyManagementService PartyManagementServiceMock;
@@ -46,7 +58,7 @@ class DashboardControllerTest {
                 });
         // when
         MvcResult result = mvc.perform(MockMvcRequestBuilders
-                        .get(BASE_URL + "/id")
+                        .get(BASE_URL + "/organization/organizationId")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
@@ -63,13 +75,56 @@ class DashboardControllerTest {
                 .thenReturn(null);
         // when
         MvcResult result = mvc.perform(MockMvcRequestBuilders
-                        .get(BASE_URL + "/id")
+                        .get(BASE_URL + "/organization/organizationId")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andReturn();
         // then
         assertEquals("", result.getResponse().getContentAsString());
+    }
+
+
+    @Test
+    void getProductsNotNull() throws Exception {
+        // given
+        Mockito.when(productsService.getProducts())
+                .thenReturn(Collections.singletonList(PRODUCT));
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/products")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        List<ProductsResource> products = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                new TypeReference<>() {
+                });
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
+    }
+
+    @Test
+    void getProductsNull() throws Exception {
+        // given
+        Mockito.when(productsService.getProducts())
+                .thenReturn(Collections.emptyList());
+        // when
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .get(BASE_URL + "/products")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        // then
+        List<ProductsResource> products = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                new TypeReference<>() {
+                });
+        assertNotNull(products);
+        assertTrue(products.isEmpty());
     }
 
 }
