81318ef5b31e38e70b3406413f75ce09b241f03e
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-11-05 16:10:25 +0100

    removed mock

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/security/PartyAuthenticationProvider.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/security/PartyAuthenticationProvider.java
@@ -35,29 +35,18 @@ public class PartyAuthenticationProvider extends AbstractUserDetailsAuthenticati
 
     @Override
     protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        User user = null;
+
         if (SelfCareAuthenticationDetails.class.isAssignableFrom(authentication.getDetails().getClass())) {
             SelfCareAuthenticationDetails details = (SelfCareAuthenticationDetails) authentication.getDetails();
-        }
-        // TODO: remove mock
-//        OnBoardingInfo onBoardingInfo = restClient.getOnBoardingInfo(details.getInstitutionId());
+            OnBoardingInfo onBoardingInfo = restClient.getOnBoardingInfo(details.getInstitutionId());
 
-        // start mock
-        InstitutionInfo instInfo = new InstitutionInfo();
-        instInfo.setDescription("Description");
-        instInfo.setDigitalAddress("DigitalAddress");
-        instInfo.setInstitutionId("InstitutionId");
-        instInfo.setRole("Manager");
-        instInfo.setPlatformRole("ADMIN");
-        OnBoardingInfo onBoardingInfo = new OnBoardingInfo();
-        onBoardingInfo.setInstitutions(Collections.singletonList(instInfo));
-        // end mock
-
-        User user = null;
-        if (onBoardingInfo != null && !onBoardingInfo.getInstitutions().isEmpty()) {
-            InstitutionInfo institutionInfo = onBoardingInfo.getInstitutions().get(0);
-            String role = institutionInfo.getPlatformRole();
-            List<SelfCareGrantedAuthority> authorities = Collections.singletonList(new SelfCareGrantedAuthority(role));
-            user = new User(username, authentication.getCredentials().toString(), authorities);
+            if (onBoardingInfo != null && !onBoardingInfo.getInstitutions().isEmpty()) {
+                InstitutionInfo institutionInfo = onBoardingInfo.getInstitutions().get(0);
+                String role = institutionInfo.getPlatformRole();
+                List<SelfCareGrantedAuthority> authorities = Collections.singletonList(new SelfCareGrantedAuthority(role));
+                user = new User(username, authentication.getCredentials().toString(), authorities);
+            }
         }
 
         return user;
