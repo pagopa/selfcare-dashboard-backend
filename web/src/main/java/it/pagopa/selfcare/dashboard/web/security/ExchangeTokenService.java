55cd45f4ed397e3286fdec23a025e668be2c9355
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-12-02 18:55:31 +0100

    fix private key parse error

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/security/ExchangeTokenService.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/security/ExchangeTokenService.java
@@ -8,6 +8,7 @@ import it.pagopa.selfcare.commons.base.security.SelfCareAuthenticationDetails;
 import it.pagopa.selfcare.commons.web.security.JwtService;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.asn1.pkcs.RSAPrivateKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +19,9 @@ import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateCrtKeySpec;
 import java.time.Duration;
 import java.util.*;
 
@@ -27,6 +30,8 @@ import java.util.*;
 public class ExchangeTokenService {
 
     private static final String ISSUER = "api.selfcare.pagopa.it";
+    private static final String PRIVATE_KEY_HEADER_TEMPLATE = "-----BEGIN %s-----";
+    private static final String PRIVATE_KEY_FOOTER_TEMPLATE = "-----END %s-----";
 
     private final PrivateKey jwtSigningKey;
     private final JwtService jwtService;
@@ -54,8 +59,9 @@ public class ExchangeTokenService {
         String institutionId = ((SelfCareAuthenticationDetails) authentication.getDetails()).getInstitutionId();
         Optional<Claims> selcClaims = jwtService.getClaims(authentication.getCredentials().toString());
         if (selcClaims.isEmpty()) {
-            throw new RuntimeException();
+            throw new RuntimeException();//TODO: define a custom exception
         }
+        System.out.println("selcClaims.get().setIssuedAt() = " + selcClaims.get().getIssuedAt());//FIXME:remove
         TokenExchangeClaims claims = new TokenExchangeClaims(selcClaims.get());
         claims.setId(UUID.randomUUID().toString());
         claims.setAudience(realm);
@@ -64,6 +70,7 @@ public class ExchangeTokenService {
         claims.setDesiredExpiration(claims.getExpiration());
         claims.setIssuedAt(new Date());
         claims.setExpiration(Date.from(claims.getIssuedAt().toInstant().plus(duration)));
+        System.out.println("claims.getIssuedAt() = " + claims.getIssuedAt());//FIXME:remove
 
         if (log.isDebugEnabled()) {
             log.debug("Exchanged claims: " + claims.toString());
@@ -77,15 +84,34 @@ public class ExchangeTokenService {
 
 
     private PrivateKey getPrivateKey(String signingKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        boolean isRsa = signingKey.contains("RSA");
+        String privateKeyEnvelopName = (isRsa ? "RSA " : "") + "PRIVATE KEY";
         String privateKeyPEM = signingKey
-                .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replaceAll(System.lineSeparator(), "")
-                .replace("-----END PRIVATE KEY-----", "");
+                .replaceAll("\\r", "")
+                .replaceAll("\\n", "")
+                .replace(String.format(PRIVATE_KEY_HEADER_TEMPLATE, privateKeyEnvelopName), "")
+                .replace(String.format(PRIVATE_KEY_FOOTER_TEMPLATE, privateKeyEnvelopName), "");
 
-        byte[] encoded = Base64.getMimeDecoder().decode(privateKeyPEM);
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+
+        KeySpec keySpec;
+        if (isRsa) {
+            RSAPrivateKey rsaPrivateKey = RSAPrivateKey.getInstance(encoded);
+            keySpec = new RSAPrivateCrtKeySpec(
+                    rsaPrivateKey.getModulus(),
+                    rsaPrivateKey.getPublicExponent(),
+                    rsaPrivateKey.getPrivateExponent(),
+                    rsaPrivateKey.getPrime1(),
+                    rsaPrivateKey.getPrime2(),
+                    rsaPrivateKey.getExponent1(),
+                    rsaPrivateKey.getExponent2(),
+                    rsaPrivateKey.getCoefficient());
+
+        } else {
+            keySpec = new PKCS8EncodedKeySpec(encoded);
+        }
 
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
         return keyFactory.generatePrivate(keySpec);
     }
 
