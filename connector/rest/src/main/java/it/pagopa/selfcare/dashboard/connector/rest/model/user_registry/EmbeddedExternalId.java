d68f27222c531e58472387a85aa843d17a64a060
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-02-09 17:59:43 +0100

    add User registry

---
+++ connector/rest/src/main/java/it/pagopa/selfcare/dashboard/connector/rest/model/user_registry/EmbeddedExternalId.java
@@ -0,0 +1,14 @@
+
+package it.pagopa.selfcare.dashboard.connector.rest.model.user_registry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EmbeddedExternalId {
+
+  private String externalId;
+}
