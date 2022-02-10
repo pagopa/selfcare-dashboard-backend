ea195e4e27dc77c89dbf81ae5185217722f82f9e
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-02-10 15:12:59 +0100

    add trace and log debug

--- core/src/main/java/it/pagopa/selfcare/dashboard/core/RelationshipServiceImpl.java
+++ core/src/main/java/it/pagopa/selfcare/dashboard/core/RelationshipServiceImpl.java
@@ -1,10 +1,12 @@
 package it.pagopa.selfcare.dashboard.core;
 
 import it.pagopa.selfcare.dashboard.connector.api.PartyConnector;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+@Slf4j
 @Service
 class RelationshipServiceImpl implements RelationshipService {
 
@@ -20,23 +22,32 @@ class RelationshipServiceImpl implements RelationshipService {
     @Override
     public void suspend(String relationshipId) {
         Assert.hasText(relationshipId, REQUIRED_RELATIONSHIP_MESSAGE);
-
+        log.trace("suspend start");
+        log.debug("suspend relationshipId = {}", relationshipId);
         partyConnector.suspend(relationshipId);
+        log.trace("suspend end");
+
     }
 
 
     @Override
     public void activate(String relationshipId) {
         Assert.hasText(relationshipId, REQUIRED_RELATIONSHIP_MESSAGE);
-
+        log.trace("activate start");
+        log.debug("activate relationshipId = {}", relationshipId);
         partyConnector.activate(relationshipId);
+        log.trace("activate end");
+
     }
 
     @Override
     public void delete(String relationshipId) {
         Assert.hasText(relationshipId, REQUIRED_RELATIONSHIP_MESSAGE);
-
+        log.trace("delete start");
+        log.debug("relationshipId = {}", relationshipId);
         partyConnector.delete(relationshipId);
+        log.trace("delete end");
+
     }
 
 
