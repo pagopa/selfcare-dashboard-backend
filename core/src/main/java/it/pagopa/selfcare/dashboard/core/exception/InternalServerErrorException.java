380c2880536242daf5f74d4f2af3356a40c5b424
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-03-24 20:25:35 +0100

    removed unnecessary exception

--- core/src/main/java/it/pagopa/selfcare/dashboard/core/exception/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package it.pagopa.selfcare.dashboard.core.exception;
-
-public class InternalServerErrorException extends RuntimeException {
-    public InternalServerErrorException(String msg) {
-        super(msg);
-    }
-}
