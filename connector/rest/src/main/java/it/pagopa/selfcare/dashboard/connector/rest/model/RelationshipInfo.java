66f44c0e4b173144e37f5a92d672312065d128bf
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-03-25 10:06:33 +0100

    workaround fix

--- connector/rest/src/main/java/it/pagopa/selfcare/dashboard/connector/rest/model/RelationshipInfo.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/dashboard/connector/rest/model/RelationshipInfo.java
@@ -29,7 +29,7 @@ public class RelationshipInfo {
     private String to;
     private String name;
     private String surname;
-    private String taxCode = "LQNSNV80P25B265F";
+    private String taxCode;
     private Certification certification;
     private Map<String, InstitutionContact> institutionContacts;
     private String email;
