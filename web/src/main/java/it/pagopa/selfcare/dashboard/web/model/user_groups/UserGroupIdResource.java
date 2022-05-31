ca7637852e85310501148d7251c3d4c2f5f69a69
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-05-31 17:52:50 +0200

    add description to user group id resource field

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/model/user_groups/UserGroupIdResource.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/model/user_groups/UserGroupIdResource.java
@@ -1,11 +1,16 @@
 package it.pagopa.selfcare.dashboard.web.model.user_groups;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserGroupIdResource {
+
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.id}", required = true)
+    @JsonProperty(required = true)
     @NotBlank
     private String id;
 }
