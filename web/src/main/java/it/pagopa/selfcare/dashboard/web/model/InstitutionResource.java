998ddb349938f7234e36d0ef92ccaee8d68d2c9a
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-06-13 18:20:00 +0200

    make institutionType optional

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/model/InstitutionResource.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/model/InstitutionResource.java
@@ -6,7 +6,6 @@ import it.pagopa.selfcare.dashboard.connector.model.institution.InstitutionType;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class InstitutionResource {
@@ -31,9 +30,7 @@ public class InstitutionResource {
     @NotBlank
     private String origin;
 
-    @ApiModelProperty(value = "${swagger.dashboard.institutions.model.institutionType}", required = true)
-    @JsonProperty(required = true)
-    @NotNull
+    @ApiModelProperty(value = "${swagger.dashboard.institutions.model.institutionType}")
     private InstitutionType institutionType;
 
     @ApiModelProperty(value = "${swagger.dashboard.institutions.model.name}", required = true)
