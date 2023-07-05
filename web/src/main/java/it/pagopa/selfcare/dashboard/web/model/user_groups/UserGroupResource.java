0e98de4252c8181551620387a21aacb517a61616
pierpaolodidato89 <pierpaolo.didato@emeal.nttdata.com>
2023-07-05 10:36:49 +0200

    Removed validation constraints on user resource

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/model/user_groups/UserGroupResource.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/model/user_groups/UserGroupResource.java
@@ -1,6 +1,5 @@
 package it.pagopa.selfcare.dashboard.web.model.user_groups;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import it.pagopa.selfcare.dashboard.connector.model.groups.UserGroupStatus;
 import it.pagopa.selfcare.dashboard.web.model.product.ProductUserResource;
@@ -11,48 +10,37 @@ import java.util.List;
 
 @Data
 public class UserGroupResource {
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.id}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.id}")
     private String id;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.institutionId}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.institutionId}")
     private String institutionId;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.productId}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.productId}")
     private String productId;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.name}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.name}")
     private String name;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.description}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.description}")
     private String description;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.status}", required = true)
-    @JsonProperty(required = true)
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.status}")
     private UserGroupStatus status;
 
-    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.members}", required = true)
-    @JsonProperty()
+    @ApiModelProperty(value = "${swagger.dashboard.user-group.model.members}")
     private List<ProductUserResource> members;
 
     @ApiModelProperty(value = "${swagger.dashboard.user-group.model.createdAt}")
-    @JsonProperty
     private Instant createdAt;
 
     @ApiModelProperty(value = "${swagger.dashboard.user-group.model.createdBy}")
-    @JsonProperty
     private PlainUserResource createdBy;
 
     @ApiModelProperty(value = "${swagger.dashboard.user-group.model.modifiedAt}")
-    @JsonProperty
     private Instant modifiedAt;
 
     @ApiModelProperty(value = "${swagger.dashboard.user-group.model.modifiedBy}")
-    @JsonProperty
     private PlainUserResource modifiedBy;
 
 }
