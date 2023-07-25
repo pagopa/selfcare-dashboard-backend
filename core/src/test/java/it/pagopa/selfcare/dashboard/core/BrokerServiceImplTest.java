c6ebc05e51fdfc23f35f4db7ee1770f9ec7ecb50
pierpaolodidato89 <pierpaolo.didato@emeal.nttdata.com>
2023-07-25 16:19:46 +0200

    Added junit tests on interceptor and service

--- core/src/test/java/it/pagopa/selfcare/dashboard/core/BrokerServiceImplTest.java
+++ core/src/test/java/it/pagopa/selfcare/dashboard/core/BrokerServiceImplTest.java
@@ -1,5 +1,6 @@
 package it.pagopa.selfcare.dashboard.core;
 
+import it.pagopa.selfcare.dashboard.connector.api.MsCoreConnector;
 import it.pagopa.selfcare.dashboard.connector.api.PagoPABackOfficeConnector;
 import it.pagopa.selfcare.dashboard.connector.model.backoffice.BrokerInfo;
 import org.junit.jupiter.api.Assertions;
@@ -13,11 +14,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+
 @ExtendWith(MockitoExtension.class)
 class BrokerServiceImplTest {
 
     @Mock
     private PagoPABackOfficeConnector backOfficeConnectorMock;
+
+    @Mock
+    private MsCoreConnector msCoreConnector;
 
     @InjectMocks
     private BrokerServiceImpl brokerService;
@@ -27,7 +34,7 @@ class BrokerServiceImplTest {
         // given
         String institutionType = "EC";
         List<BrokerInfo> brokersMocked = buildBrokerInfos();
-        Mockito.when(backOfficeConnectorMock.getBrokersEC(Mockito.anyInt(), Mockito.anyInt()))
+        Mockito.when(backOfficeConnectorMock.getBrokersEC(anyInt(), anyInt()))
                 .thenReturn(brokersMocked);
         // when
         List<BrokerInfo> brokers = brokerService.findAllByInstitutionType(institutionType);
@@ -44,7 +51,7 @@ class BrokerServiceImplTest {
         // given
         String institutionType = "PSP";
         List<BrokerInfo> brokersMocked = buildBrokerInfos();
-        Mockito.when(backOfficeConnectorMock.getBrokersPSP(Mockito.anyInt(), Mockito.anyInt()))
+        Mockito.when(backOfficeConnectorMock.getBrokersPSP(anyInt(), anyInt()))
                 .thenReturn(brokersMocked);
         // when
         List<BrokerInfo> brokers = brokerService.findAllByInstitutionType(institutionType);
@@ -53,6 +60,24 @@ class BrokerServiceImplTest {
         Mockito.verify(backOfficeConnectorMock, Mockito.times(1))
                 .getBrokersPSP(1,1000);
         Mockito.verifyNoMoreInteractions(backOfficeConnectorMock);
+
+    }
+
+    @Test
+    void findInstitutionsByProductAndType() {
+        // given
+        final String institutionType = "EC";
+        final String productId = "productId";
+        List<BrokerInfo> brokersMocked = buildBrokerInfos();
+        Mockito.when(msCoreConnector.findInstitutionsByProductAndType(anyString(), anyString()))
+                .thenReturn(brokersMocked);
+        // when
+        List<BrokerInfo> brokers = brokerService.findInstitutionsByProductAndType(productId, institutionType);
+        // then
+        Assertions.assertSame(brokersMocked, brokers);
+        Mockito.verify(msCoreConnector, Mockito.times(1))
+                .findInstitutionsByProductAndType(productId,institutionType);
+        Mockito.verifyNoMoreInteractions(msCoreConnector);
 
     }
 
