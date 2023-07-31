b166424130cfdb96e60e16772aeeca3ae9876439
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2023-07-31 10:34:36 +0200

    feat: added service to get zendesk url for contact form (#234)
    * Created service to get zendesk url for contact form
    
    * Update dependency for sign zendesk jwt

---
+++ connector-api/src/main/java/it/pagopa/selfcare/dashboard/connector/model/support/SupportRequest.java
@@ -0,0 +1,12 @@
+package it.pagopa.selfcare.dashboard.connector.model.support;
+
+import lombok.Data;
+
+@Data
+public class SupportRequest {
+
+    private String email;
+    private String name;
+    private UserField userFields;
+
+}
