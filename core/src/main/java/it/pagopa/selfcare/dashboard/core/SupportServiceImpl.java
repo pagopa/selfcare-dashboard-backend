13dd7d9754950a51cc62bccddfdd2c721707f59c
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-02-06 17:08:44 +0100

    Added userId and institutionId into SupportRequest

--- core/src/main/java/it/pagopa/selfcare/dashboard/core/SupportServiceImpl.java
+++ core/src/main/java/it/pagopa/selfcare/dashboard/core/SupportServiceImpl.java
@@ -3,18 +3,23 @@ package it.pagopa.selfcare.dashboard.core;
 import freemarker.template.Template;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import it.pagopa.selfcare.dashboard.connector.api.UserRegistryConnector;
+import it.pagopa.selfcare.dashboard.connector.exception.ResourceNotFoundException;
 import it.pagopa.selfcare.dashboard.connector.exception.SupportException;
 import it.pagopa.selfcare.dashboard.connector.model.support.SupportRequest;
+import it.pagopa.selfcare.dashboard.connector.model.support.UserField;
+import it.pagopa.selfcare.dashboard.connector.model.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-
 
 import java.util.*;
 
+import static it.pagopa.selfcare.dashboard.connector.model.user.User.Fields.*;
 
 @Slf4j
 @Service
@@ -23,20 +28,24 @@ public class SupportServiceImpl implements SupportService {
     private final String supportApiKey;
     private final String zendeskOrganization;
     private final String returnTo;
-    private final String redirectUrl;
+    private final String actionUrl;
+    private final UserRegistryConnector userRegistryConnector;
     @Qualifier("zendeskFreeMarker")
     private final FreeMarkerConfigurer freeMarkerConfigurer;
+    private static final EnumSet<User.Fields> USER_FIELD_LIST = EnumSet.of(name, familyName, fiscalCode);
 
     public SupportServiceImpl(@Value("${support.api.key}") String supportApiKey,
                               @Value("${support.api.zendesk.redirectUri}") String returnTo,
                               @Value("${support.api.zendesk.organization}") String zendeskOrganization,
                               @Value("${support.api.zendesk.actionUri}") String redirectUrl,
-                              FreeMarkerConfigurer freeMarkerConfigurer) {
+                              FreeMarkerConfigurer freeMarkerConfigurer,
+                              UserRegistryConnector userRegistryConnector) {
         this.supportApiKey = supportApiKey;
         this.returnTo = returnTo;
         this.zendeskOrganization = zendeskOrganization;
-        this.redirectUrl = redirectUrl;
+        this.actionUrl = redirectUrl;
         this.freeMarkerConfigurer = freeMarkerConfigurer;
+        this.userRegistryConnector = userRegistryConnector;
     }
 
     @Override
@@ -44,6 +53,42 @@ public class SupportServiceImpl implements SupportService {
 
         log.trace("sendRequest start");
         log.debug("sendRequest request = {}", supportRequest);
+
+        if(StringUtils.hasText(supportRequest.getUserId())) {
+            try {
+                User user = userRegistryConnector.getUserByInternalId(supportRequest.getUserId(), USER_FIELD_LIST);
+                supportRequest.setName(user.getName().getValue().concat(" " + user.getFamilyName().getValue()));
+                supportRequest.setUserFields(UserField.builder().aux_data(user.getFiscalCode()).build());
+            } catch (Exception e) {
+                throw new ResourceNotFoundException("User with id " + supportRequest.getUserId() + " not found");
+            }
+        }
+
+        //Retrieve parameters for submitting form
+        String redirectUrl = getRedirectUrl(supportRequest);
+        String jwtString = getJWTString(supportRequest);
+
+        String html;
+        try {
+            Map<String, String> map = Map.of(
+                    "jwt", jwtString,
+                    "returnTo", redirectUrl,
+                    "action", actionUrl
+            );
+            Template freemarkerTemplate = freeMarkerConfigurer.getConfiguration()
+                    .getTemplate("/template-zendesk-form.ftl");
+            html = FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerTemplate, map);
+        } catch (Exception e){
+            String errorMessage = "Impossible to retrieve zendesk form template";
+            log.error(errorMessage);
+            throw new SupportException(errorMessage, e);
+        }
+
+        log.trace("sendRequest end");
+        return html;
+    }
+
+    private String getJWTString(SupportRequest supportRequest) {
         String jwtString;
         try {
             jwtString = Jwts.builder()
@@ -59,28 +104,19 @@ public class SupportServiceImpl implements SupportService {
             log.error("Impossible to sign zendesk jwt. Error: {}", e.getMessage(), e);
             throw new SupportException(e.getMessage());
         }
+        return jwtString;
+    }
 
-        String redirectToProduct = Objects.nonNull(supportRequest.getProductId()) ?
-                returnTo.concat("?product=" + supportRequest.getProductId()) : returnTo;
-
-        Map<String, String> map = new HashMap<>();
-        map.put("jwt", jwtString);
-        map.put("returnTo", redirectToProduct);
-        map.put("action", redirectUrl);
-
-        String html;
-
-        try {
-            Template freemarkerTemplate = freeMarkerConfigurer.getConfiguration()
-                    .getTemplate("/template-zendesk-form.ftl");
-            html = FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerTemplate, map);
-        } catch (Exception e){
-            String errorMessage = "Impossible to retrieve zendesk form template";
-            log.error(errorMessage);
-            throw new SupportException(errorMessage, e);
+    private String getRedirectUrl(SupportRequest supportRequest) {
+        StringBuilder urlBuilder = new StringBuilder(returnTo);
+        if(Objects.nonNull(supportRequest.getProductId())) {
+            urlBuilder.append("?product=").append(supportRequest.getProductId());
         }
-
-        log.trace("sendRequest end");
-        return html;
+        if(Objects.nonNull(supportRequest.getInstitutionId())) {
+            urlBuilder.append(urlBuilder.indexOf("?") != -1 ?
+                    "&institution=" + supportRequest.getInstitutionId()
+                    : "?product=" + supportRequest.getInstitutionId());
+        }
+        return urlBuilder.toString();
     }
 }
--- core/src/test/java/it/pagopa/selfcare/dashboard/core/SupportServiceImplTest.java
+++ core/src/test/java/it/pagopa/selfcare/dashboard/core/SupportServiceImplTest.java
@@ -2,19 +2,32 @@ package it.pagopa.selfcare.dashboard.core;
 
 import freemarker.cache.ClassTemplateLoader;
 import freemarker.cache.TemplateLoader;
+import it.pagopa.selfcare.dashboard.connector.api.UserRegistryConnector;
+import it.pagopa.selfcare.dashboard.connector.exception.ResourceNotFoundException;
 import it.pagopa.selfcare.dashboard.connector.exception.SupportException;
 import it.pagopa.selfcare.dashboard.connector.model.support.SupportRequest;
 import it.pagopa.selfcare.dashboard.connector.model.support.UserField;
+import it.pagopa.selfcare.dashboard.connector.model.user.CertifiedField;
+import it.pagopa.selfcare.dashboard.connector.model.user.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 class SupportServiceImplTest {
+
+    @Mock
+    private UserRegistryConnector userRegistryConnector;
 
     /**
      * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
@@ -22,7 +35,7 @@ class SupportServiceImplTest {
     @Test
     void testSendRequest(){
         FreeMarkerConfigurer freeMarkerConfigurer = freemarkerClassLoaderConfig();
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer);
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer, null);
         String url = supportServiceImpl.sendRequest(this.dummySupportRequest());
         assertNotNull(url);
         assertTrue(url.contains("jwt"));
@@ -33,9 +46,47 @@ class SupportServiceImplTest {
      * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
      */
     @Test
+    void testSendRequestWithUserId(){
+        FreeMarkerConfigurer freeMarkerConfigurer = freemarkerClassLoaderConfig();
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer, userRegistryConnector);
+        SupportRequest supportRequest = this.dummySupportRequest();
+        supportRequest.setUserId("userId");
+        supportRequest.setInstitutionId("institutionId");
+        User user = new User();
+        user.setFiscalCode("fiscalCode");
+        CertifiedField<String> certifiedField = new CertifiedField<>();
+        certifiedField.setValue("name");
+        user.setName(certifiedField);
+        user.setFamilyName(certifiedField);
+        when(userRegistryConnector.getUserByInternalId(anyString(), any())).thenReturn(user);
+        String url = supportServiceImpl.sendRequest(supportRequest);
+        assertNotNull(url);
+        assertTrue(url.contains("jwt"));
+        assertEquals("<html>", url.substring(0, 6));
+    }
+
+    /**
+     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
+     */
+    @Test
+    void testSendRequestUserNotFound(){
+        FreeMarkerConfigurer freeMarkerConfigurer = freemarkerClassLoaderConfig();
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer, null);
+        SupportRequest supportRequest = this.dummySupportRequest();
+        supportRequest.setUserId("null");
+        Executable executable = () -> supportServiceImpl.sendRequest(supportRequest);
+        // then
+        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class, executable);
+        Assertions.assertEquals("User with id null not found", e.getMessage());
+    }
+
+    /**
+     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
+     */
+    @Test
     void testSendRequestThrowException() {
         FreeMarkerConfigurer freeMarkerConfigurer = Mockito.mock(FreeMarkerConfigurer.class);
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer);
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", freeMarkerConfigurer, null);
         Exception exception = assertThrows(Exception.class, () -> supportServiceImpl.sendRequest(this.dummySupportRequest()));
         assertEquals("Impossible to retrieve zendesk form template", exception.getMessage());
     }
@@ -46,7 +97,7 @@ class SupportServiceImplTest {
     @Test
     void testSendRequestWithProductId() {
         FreeMarkerConfigurer freeMarkerConfigurer = freemarkerClassLoaderConfig();
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "", "redirectUrl", freeMarkerConfigurer);
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "", "redirectUrl", freeMarkerConfigurer, null);
         SupportRequest supportRequest = this.dummySupportRequest();
         supportRequest.setProductId("prodottoDiTest");
         String url = supportServiceImpl.sendRequest(supportRequest);
@@ -60,7 +111,7 @@ class SupportServiceImplTest {
      */
     @Test
     void testRequestWithMalformedEmptyKey() {
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("", "", "test", "redirectUrl", null);
+        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("", "", "test", "redirectUrl", null, null);
         SupportException exception = assertThrows(SupportException.class, () -> supportServiceImpl.sendRequest(this.dummySupportRequest()));
         assertEquals("secret key byte array cannot be null or empty.", exception.getMessage());
     }
