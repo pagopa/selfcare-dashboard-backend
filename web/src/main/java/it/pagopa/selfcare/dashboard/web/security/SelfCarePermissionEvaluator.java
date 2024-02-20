b55f4a89fd4ba342b7fc5272a034e89861daf4e9
flaminiaScarciofolo <113031535+flaminiaScarciofolo@users.noreply.github.com>
2024-02-20 14:28:56 +0100

    [SELC-4326] feat: added check permission in operation with relationshipId (#353)
    * add check permission in operation with relationshipId
    
    * fix userMapper
    
    * add tests for SelfCarePermissionEvaluator

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/security/SelfCarePermissionEvaluator.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/security/SelfCarePermissionEvaluator.java
@@ -2,11 +2,14 @@ package it.pagopa.selfcare.dashboard.web.security;
 
 import it.pagopa.selfcare.commons.base.logging.LogUtils;
 import it.pagopa.selfcare.commons.base.security.SelfCareGrantedAuthority;
+import it.pagopa.selfcare.dashboard.connector.api.MsCoreConnector;
+import it.pagopa.selfcare.dashboard.connector.model.user.UserInfo;
 import it.pagopa.selfcare.dashboard.web.model.InstitutionResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
 
 import java.io.Serializable;
 
@@ -14,6 +17,12 @@ import java.io.Serializable;
 public class SelfCarePermissionEvaluator implements PermissionEvaluator {
 
     public static final String ANY_PERMISSION = "ANY";
+    public static final String RELATIONSHIP_ID = "relationshipId";
+    private final MsCoreConnector msCoreConnector;
+
+    public SelfCarePermissionEvaluator(MsCoreConnector msCoreConnector) {
+        this.msCoreConnector = msCoreConnector;
+    }
 
 
     @Override
@@ -62,6 +71,21 @@ public class SelfCarePermissionEvaluator implements PermissionEvaluator {
                     .map(SelfCareGrantedAuthority.class::cast)
                     .filter(grantedAuthority -> targetId.toString().equals(grantedAuthority.getInstitutionId()))
                     .anyMatch(grantedAuthority -> ANY_PERMISSION.equals(permission) || permission.equals(grantedAuthority.getAuthority()));
+        }
+
+        if (targetId != null && RELATIONSHIP_ID.equals(targetType)) {
+            UserInfo userInfo = msCoreConnector.getUser(targetId.toString());
+            if(!CollectionUtils.isEmpty(userInfo.getProducts()) && userInfo.getProducts().size() == 1) {
+                String productId = userInfo.getProducts().keySet().stream().toList().get(0);
+                result = authentication.getAuthorities()
+                        .stream()
+                        .filter(grantedAuthority -> SelfCareGrantedAuthority.class.isAssignableFrom(grantedAuthority.getClass()))
+                        .map(SelfCareGrantedAuthority.class::cast)
+                        .filter(grantedAuthority -> grantedAuthority.getInstitutionId().equals(userInfo.getInstitutionId()))
+                        .filter(grantedAuthority -> grantedAuthority.getRoleOnProducts().containsKey(productId))
+                        .map(grantedAuthority -> grantedAuthority.getRoleOnProducts().get(productId))
+                        .anyMatch(grantedAuthority -> ANY_PERMISSION.equals(permission) || permission.equals(grantedAuthority.getAuthority()));
+            }
         }
 
         log.debug("hasPermission result = {}", result);
