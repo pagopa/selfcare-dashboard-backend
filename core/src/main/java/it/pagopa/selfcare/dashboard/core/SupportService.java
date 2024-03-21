e35bb0987da1764683d6824bafa9805c3993ddc3
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2024-03-21 10:22:44 +0100

    [SELC-4535] feat: added endpoint for zendesk integraton to fix error in pnpg

--- core/src/main/java/it/pagopa/selfcare/dashboard/core/SupportService.java
+++ core/src/main/java/it/pagopa/selfcare/dashboard/core/SupportService.java
@@ -3,6 +3,6 @@ package it.pagopa.selfcare.dashboard.core;
 import it.pagopa.selfcare.dashboard.connector.model.support.SupportRequest;
 
 public interface SupportService {
-
     String sendRequest(SupportRequest supportRequest);
+    String getSupportRequest(SupportRequest supportRequest);
 }
