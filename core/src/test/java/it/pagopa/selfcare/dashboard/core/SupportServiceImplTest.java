fb6133c19daee120f4586315400d8f219ad8a15b
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-06-13 09:38:13 +0200

    [SELC-4733] Refactor test for core module

--- core/src/test/java/it/pagopa/selfcare/dashboard/core/SupportServiceImplTest.java
+++ core/src/test/java/it/pagopa/selfcare/dashboard/core/SupportServiceImplTest.java
@@ -1,115 +1,111 @@
 package it.pagopa.selfcare.dashboard.core;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import it.pagopa.selfcare.dashboard.connector.api.UserRegistryConnector;
 import it.pagopa.selfcare.dashboard.connector.exception.ResourceNotFoundException;
 import it.pagopa.selfcare.dashboard.connector.exception.SupportException;
 import it.pagopa.selfcare.dashboard.connector.model.support.SupportRequest;
 import it.pagopa.selfcare.dashboard.connector.model.support.SupportResponse;
-import it.pagopa.selfcare.dashboard.connector.model.support.UserField;
-import it.pagopa.selfcare.dashboard.connector.model.user.CertifiedField;
 import it.pagopa.selfcare.dashboard.connector.model.user.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.EnumSet;
+
+import static it.pagopa.selfcare.dashboard.connector.model.user.User.Fields.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-@ExtendWith({MockitoExtension.class})
-class SupportServiceImplTest {
+@ExtendWith(MockitoExtension.class)
+public class SupportServiceImplTest extends BaseServiceTest {
 
+    @InjectMocks
+    SupportServiceImpl supportServiceImpl;
     @Mock
-    private UserRegistryConnector userRegistryConnector;
+    private UserRegistryConnector userRegistryConnectorMock;
+    private static final EnumSet<User.Fields> USER_FIELD_LIST = EnumSet.of(name, familyName, fiscalCode);
 
-    /**
-     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
-     */
-    @Test
-    void testSendRequest(){
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "http://www.test.it", "test-organization", "redirectUrl", null);
-        SupportResponse response = supportServiceImpl.sendRequest(this.dummySupportRequest());
-        assertNotNull(response);
-        assertNotNull(response.getJwt());
-        assertNotNull(response.getRedirectUrl());
-        assertEquals("http", response.getRedirectUrl().substring(0, 4));
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
     }
 
-    /**
-     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
-     */
     @Test
-    void testSendRequestWithUserId(){
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "http://www.test.it", "test-organization", "redirectUrl", userRegistryConnector);
-        SupportRequest supportRequest = this.dummySupportRequest();
-        supportRequest.setUserId("userId");
-        supportRequest.setInstitutionId("institutionId");
-        User user = new User();
-        user.setFiscalCode("fiscalCode");
-        CertifiedField<String> certifiedField = new CertifiedField<>();
-        certifiedField.setValue("name");
-        user.setName(certifiedField);
-        user.setFamilyName(certifiedField);
-        when(userRegistryConnector.getUserByInternalId(anyString(), any())).thenReturn(user);
+    void sendRequest() throws IOException {
+
+        ReflectionTestUtils.setField(supportServiceImpl, "supportApiKey", "testSupportApiKey");
+        ReflectionTestUtils.setField(supportServiceImpl, "returnTo", "testReturnTo");
+        ReflectionTestUtils.setField(supportServiceImpl, "zendeskOrganization", "testZendeskOrganization");
+        ReflectionTestUtils.setField(supportServiceImpl, "actionUrl", "testActionUrl");
+
+        SupportResponse expectation = new SupportResponse();
+        expectation.setRedirectUrl("testReturnTo?product=productId&institution=institutionId");
+        expectation.setActionUrl("testActionUrl");
+
+
+        ClassPathResource resource = new ClassPathResource("expectations/SupportRequest.json");
+        byte[] supportRequestStream = Files.readAllBytes(resource.getFile().toPath());
+        SupportRequest supportRequest = objectMapper.readValue(supportRequestStream, new TypeReference<>() {
+        });
+
+        ClassPathResource userResource = new ClassPathResource("expectations/User.json");
+        byte[] userStream = Files.readAllBytes(userResource.getFile().toPath());
+        User user = objectMapper.readValue(userStream, new TypeReference<>() {
+        });
+
+        when(userRegistryConnectorMock.getUserByInternalId(supportRequest.getUserId(), USER_FIELD_LIST)).thenReturn(user);
+
         SupportResponse response = supportServiceImpl.sendRequest(supportRequest);
-        assertNotNull(response);
-        assertNotNull(response.getJwt());
-        assertNotNull(response.getRedirectUrl());
-        assertEquals("http", response.getRedirectUrl().substring(0, 4));
+
+        Assertions.assertEquals(expectation.getRedirectUrl(), response.getRedirectUrl());
+        Assertions.assertEquals(expectation.getActionUrl(), response.getActionUrl());
+        Assertions.assertNotNull(response.getJwt());
+        Mockito.verify(userRegistryConnectorMock, Mockito.times(1)).getUserByInternalId(supportRequest.getUserId(), USER_FIELD_LIST);
     }
 
-    /**
-     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
-     */
     @Test
-    void testSendRequestUserNotFound(){
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "", "test-organization", "redirectUrl", null);
-        SupportRequest supportRequest = this.dummySupportRequest();
-        supportRequest.setUserId("null");
-        Executable executable = () -> supportServiceImpl.sendRequest(supportRequest);
-        // then
-        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class, executable);
-        Assertions.assertEquals("User with id null not found", e.getMessage());
+    void sendRequest_userIdEmpty() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("expectations/SupportRequest.json");
+        byte[] supportRequestStream = Files.readAllBytes(resource.getFile().toPath());
+        SupportRequest supportRequest = objectMapper.readValue(supportRequestStream, new TypeReference<>() {
+        });
+
+        when(userRegistryConnectorMock.getUserByInternalId(supportRequest.getUserId(), USER_FIELD_LIST)).thenReturn(null);
+
+        assertThrows(ResourceNotFoundException.class, () -> supportServiceImpl.sendRequest(supportRequest));
     }
 
-    /**
-     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
-     */
     @Test
-    void testSendRequestWithProductId() {
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("w90kAW1FIIJaMuWbKGyd8GfDkv45tVPiyYvrdLADsK2ANX26", "http://www.test.it", "", "redirectUrl", null);
-        SupportRequest supportRequest = this.dummySupportRequest();
-        supportRequest.setProductId("prodottoDiTest");
-        SupportResponse response = supportServiceImpl.sendRequest(supportRequest);
-        assertNotNull(response);
-        assertNotNull(response.getJwt());
-        assertNotNull(response.getRedirectUrl());
-        assertEquals("http", response.getRedirectUrl().substring(0, 4));
-        assertTrue(response.getRedirectUrl().contains(supportRequest.getProductId()));
-    }
+    void sendRequest_SupportApiKeyEmpty() throws IOException {
 
-    /**
-     * Method under test: {@link SupportServiceImpl#sendRequest(SupportRequest)}
-     */
-    @Test
-    void testRequestWithMalformedEmptyKey() {
-        SupportServiceImpl supportServiceImpl = new SupportServiceImpl("", "", "test", "redirectUrl", null);
-        SupportException exception = assertThrows(SupportException.class, () -> supportServiceImpl.sendRequest(this.dummySupportRequest()));
-        assertEquals("secret key byte array cannot be null or empty.", exception.getMessage());
-    }
-    
-    private SupportRequest dummySupportRequest() {
-        SupportRequest supportRequest = new SupportRequest();
-        supportRequest.setName("Mario Rossi");
-        supportRequest.setEmail("test@gmail.com");
-        UserField userField = new UserField();
-        userField.setAux_data("PPL89M");
-        supportRequest.setUserFields(userField);
-        return supportRequest;
-    }
+        ReflectionTestUtils.setField(supportServiceImpl, "supportApiKey", null);
+        ReflectionTestUtils.setField(supportServiceImpl, "returnTo", "testReturnTo");
+        ReflectionTestUtils.setField(supportServiceImpl, "zendeskOrganization", "testZendeskOrganization");
+        ReflectionTestUtils.setField(supportServiceImpl, "actionUrl", "testActionUrl");
 
+        ClassPathResource resource = new ClassPathResource("expectations/SupportRequest.json");
+        byte[] supportRequestStream = Files.readAllBytes(resource.getFile().toPath());
+        SupportRequest supportRequest = objectMapper.readValue(supportRequestStream, new TypeReference<>() {
+        });
+
+        ClassPathResource userResource = new ClassPathResource("expectations/User.json");
+        byte[] userStream = Files.readAllBytes(userResource.getFile().toPath());
+        User user = objectMapper.readValue(userStream, new TypeReference<>() {
+        });
+
+        when(userRegistryConnectorMock.getUserByInternalId(supportRequest.getUserId(), USER_FIELD_LIST)).thenReturn(user);
+
+        assertThrows(SupportException.class, () -> supportServiceImpl.sendRequest(supportRequest));
+    }
 }
