2959a5655813fe34348458efab21dbfca290ebdd
manuraf <manuel.rafeli@pagopa.it>
2024-09-24 14:25:44 +0200

    feat: deprecated and add geographic-taxonomy path

--- web/src/main/java/it/pagopa/selfcare/dashboard/web/controller/InstitutionController.java
+++ web/src/main/java/it/pagopa/selfcare/dashboard/web/controller/InstitutionController.java
@@ -96,7 +96,29 @@ public class InstitutionController {
         return result;
     }
 
+    @Deprecated
     @PutMapping(value = "/{institutionId}/geographicTaxonomy", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "", notes = "${swagger.dashboard.institutions.api.updateInstitutionGeographicTaxonomy}")
+    @PreAuthorize("hasPermission(new it.pagopa.selfcare.dashboard.web.security.FilterAuthorityDomain(#institutionId, null, null), 'Selc:ViewInstitutionData')")
+    public void updateInstitutionGeographicTaxonomyDeprecated(@ApiParam("${swagger.dashboard.institutions.model.id}")
+                                                    @PathVariable("institutionId")
+                                                    String institutionId,
+                                                    @ApiParam("${swagger.dashboard.institutions.model.geographicTaxonomy}")
+                                                    @RequestBody
+                                                    @Valid
+                                                    GeographicTaxonomyListDto geographicTaxonomyListDto) {
+        log.trace("updateInstitutionGeographicTaxonomy start");
+        log.debug("updateInstitutionGeographicTaxonomy institutionId = {}, geographic taxonomies = {}", institutionId, geographicTaxonomyListDto);
+        GeographicTaxonomyList geographicTaxonomies = new GeographicTaxonomyList();
+        geographicTaxonomies.setGeographicTaxonomyList(geographicTaxonomyListDto.getGeographicTaxonomyDtoList().stream().map(GeographicTaxonomyMapper::fromDto).toList());
+        institutionService.updateInstitutionGeographicTaxonomy(institutionId, geographicTaxonomies);
+        log.trace("updateInstitutionsGeographicTaxonomy end");
+    }
+
+
+
+    @PutMapping(value = "/{institutionId}/geographic-taxonomy", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "", notes = "${swagger.dashboard.institutions.api.updateInstitutionGeographicTaxonomy}")
     @PreAuthorize("hasPermission(new it.pagopa.selfcare.dashboard.web.security.FilterAuthorityDomain(#institutionId, null, null), 'Selc:ViewInstitutionData')")
