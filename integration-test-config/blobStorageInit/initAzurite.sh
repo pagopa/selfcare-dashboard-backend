bc466169cff9df4324c12545df17b25785a10786
Pierluigi D'Uffizi <DFFPLG91T09H501X@sogeiesterni.it>
2025-06-20 13:05:29 +0200

    update line separator LF initAzurite.sh

--- integration-test-config/blobStorageInit/initAzurite.sh
+++ integration-test-config/blobStorageInit/initAzurite.sh
@@ -1,5 +1,6 @@
 sleep 5
 mkdir -p /workspace
+
 # Aggiungo products.json
 az storage container create --name products --account-name devstoreaccount1 --account-key Eby8vdM02xNOcqFlqUwJPLlmEtlCD9I1QhZT4gRjAAHEDPazjFIwtg== --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCD9I1QhZT4gRjAAHEDPazjFIwtg==;BlobEndpoint=http://azurite:10000/devstoreaccount1;'
 az storage blob upload --overwrite --container-name products --file ./workspace/products.json --name products.json --account-name devstoreaccount1 --account-key Eby8vdM02xNOcqFlqUwJPLlmEtlCD9I1QhZT4gRjAAHEDPazjFIwtg== --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCD9I1QhZT4gRjAAHEDPazjFIwtg==;BlobEndpoint=http://azurite:10000/devstoreaccount1;'
