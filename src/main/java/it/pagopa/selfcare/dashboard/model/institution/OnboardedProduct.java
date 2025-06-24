e95ad2f5bd512d7cb3b2ad28d3536bf0708d7f0d
Pierluigi D'Uffizi <DFFPLG91T09H501X@sogeiesterni.it>
2025-06-24 10:52:36 +0200

    update import InstitutionType enum to the institution onboarding subnode

--- src/main/java/it/pagopa/selfcare/dashboard/model/institution/OnboardedProduct.java
+++ src/main/java/it/pagopa/selfcare/dashboard/model/institution/OnboardedProduct.java
@@ -1,6 +1,6 @@
 package it.pagopa.selfcare.dashboard.model.institution;
 
-import it.pagopa.selfcare.commons.base.utils.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
 import lombok.Data;
 
 import java.util.List;
--- src/main/java/it/pagopa/selfcare/dashboard/model/user/OnboardingInfo.java
+++ src/main/java/it/pagopa/selfcare/dashboard/model/user/OnboardingInfo.java
@@ -1,7 +1,7 @@
 package it.pagopa.selfcare.dashboard.model.user;
 
 import io.swagger.annotations.ApiModelProperty;
-import it.pagopa.selfcare.commons.base.utils.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
 import lombok.Data;
 
 @Data
