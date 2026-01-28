56d274a7195e08da7505d22941252c39c2963775
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-01-28 18:56:08 +0100

    [SELC-8115] Fixed SwaggerConfigTest

--- src/test/java/it/pagopa/selfcare/dashboard/config/SwaggerConfigTest.java
+++ src/test/java/it/pagopa/selfcare/dashboard/config/SwaggerConfigTest.java
@@ -8,15 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,9 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@EnableWebMvc
-@ComponentScan(basePackages = "it.pagopa.selfcare.dashboard.controller")
-@TestPropertySource(locations = "classpath:config/application.yml")
 class SwaggerConfigTest {
 
     @MockBean
@@ -45,6 +39,9 @@ class SwaggerConfigTest {
 
     @MockBean
     private ProductService productServiceMock;
+
+    @MockBean
+    private it.pagopa.selfcare.product.service.ProductService productServiceMock2;
 
     @MockBean
     private UserV2Service userServiceV2Mock;
